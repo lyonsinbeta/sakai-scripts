f35dafdb287f3f8f9ee8f42359d7d4a5603dc846
David Lyons <davidpaullyons@gmail.com>
2012-03-26 10:42:07 -0400

    Refactored csv into 'data' variable

--- activate-courses.rb
+++ activate-courses.rb
@@ -3,7 +3,10 @@ require 'csv'
 
 host	= ''
 usr 	= ''
-pwd 	= '' 
+pwd 	= ''
+data = '.csv'
+
+### Do not edit below this line ###
 
 login_wsdl 	= "#{host}/sakai-axis/SakaiLogin.jws?wsdl"
 script_wsdl 	= "#{host}/sakai-axis/SakaiScript.jws?wsdl"
@@ -12,24 +15,24 @@ longsight_wsdl	= "#{host}/sakai-axis/WSLongsight.jws?wsdl"
 login = Savon::Client.new(login_wsdl)
 
 session = login.request(:login) do
-	soap.body = { :id => usr, :pw => pwd }
+  soap.body = { :id => usr, :pw => pwd }
 end
 
 soapClient 	= Savon::Client.new(script_wsdl)
 soapLSClient 	= Savon::Client.new(longsight_wsdl) 
 
-CSV.foreach('test_data.csv', 'r') do |row|
-	response = soapLSClient.request(:add_inactive_member_to_site_with_role) do
-		soap.body = {	:sessionid 	=> session[:login_response][:login_return],
-				   	:siteid 		=> row[0],
-					:eid	 		=> row[1],
-					:roleid 		=> 'Instructor' }
-	end
+CSV.foreach(data, 'r') do |row|
+  response = soapLSClient.request(:add_inactive_member_to_site_with_role) do
+	soap.body = {	:sessionid 	=> session[:login_response][:login_return],
+			   	:siteid 		=> row[0],
+				:eid	 		=> row[1],
+				:roleid 		=> 'Instructor' }
+  end
 
-	response = soapLSClient.request(:set_member_status) do
-		soap.body = {	:sessionid 	=> session[:login_response][:login_return],
-					:siteid	 	=> row[0],
-					:eid		 	=> row[1],
-					:active	 	=> true }
-	end
+  response = soapLSClient.request(:set_member_status) do
+	soap.body = {	:sessionid 	=> session[:login_response][:login_return],
+				:siteid	 	=> row[0],
+				:eid		 	=> row[1],
+				:active	 	=> true }
+  end
 end
